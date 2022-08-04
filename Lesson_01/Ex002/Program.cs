0df9b9369f42497cfc403c77df89fd5ec74fed67
GeorgeSaltykov <sonofpeley@gmail.com>
2022-08-04 23:25:15 +0300

    Something to do

--- Lesson_01/Ex002/Program.cs
+++ Lesson_01/Ex002/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число дня недели");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+string [] week = {"Monday", ""}
